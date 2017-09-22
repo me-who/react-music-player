4443a1f4ef6ec63281580c4514db3f501539ba52
lee <Hao_Lee7214@163.com>
2017-09-22 17:56:45 +0800

    HotModuleReplacementPlugin configuration is complete

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,8 @@ const webpack = require('webpack');
 const path=require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-	entry: [
+	entry :[
 		// 这里是你的入口文件
-    'webpack-dev-server/client?http://0.0.0.0:3000',
-    'webpack/hot/only-dev-server',
 		'./app/index.js'
 	],
 	output:{
@@ -38,15 +36,16 @@ module.exports = {
             test: /\.less/,
             loader: 'style-loader!css-loader!less-loader'
         }
-      ]
+    	]
     },
 	/*热替换*/
 	plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      inject: 'body',
-      filename: './index.html'
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ]
+		/*html-webpack-plugin插件*/
+		new HtmlWebpackPlugin({
+			template: './index.html',
+			inject :'body',
+			filename:'./index.html'
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+	]
 }
