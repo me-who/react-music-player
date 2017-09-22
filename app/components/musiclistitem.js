8f4844af27b69bcca55c2afe560b26abb092156f
lee <Hao_Lee7214@163.com>
2017-09-22 17:34:42 +0800

    add musiclist

---
+++ app/components/musiclistitem.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './musiclistitem.less'
+import Pubsub from 'pubsub-js'
+
+class MusicListItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  playMusic(musicItem) {
+    Pubsub.publish('PLAY_MUSIC', musicItem);
+  }
+  deleteMusic(musicItem, e) {
+    e.stopPropagation();
+    Pubsub.publish('DELETE_MUSIC', musicItem);
+  }
+  render() {
+    let musicItem = this.props.musicItem;
+    return (
+      <li onClick={this.playMusic.bind(this, musicItem)} className={`components-musiclistitem row${this.props.focus ? ' focus' : ''}`}>
+        <p><strong>{musicItem.title}</strong> - {musicItem.artist}</p>
+        <p onClick={this.deleteMusic.bind(this, musicItem)} className="-col-auto delete"></p>
+      </li>
+    );
+  }
+}
+export default MusicListItem;
